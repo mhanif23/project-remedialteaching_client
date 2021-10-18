dabbd5f47620b51ef245cffc8379d20021a29758
muharamrizal2 <muharamrizal2@gmail.com>
2021-10-18 21:58:58 +0700

    add box login container

--- src/page/login.tsx
+++ src/page/login.tsx
@@ -1,8 +1,29 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
 const Login = () => {
   return (
-    <>
-      <div>hello from login</div>
-    </>
+    <Grid
+      container
+      spacing={0}
+      direction='column'
+      alignItems='center'
+      justifyContent='center'
+      style={{ minHeight: '100vh' }}
+    >
+      <Grid item xs={3} md={8}>
+        <Box
+          sx={{
+            width: 500,
+            height: 500,
+            border: 2,
+            borderRadius: 5,
+            borderStyle: 'solid',
+          }}
+        />
+      </Grid>
+    </Grid>
   );
 };
 export default Login;
