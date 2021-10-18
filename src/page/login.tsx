9ec4dad2fafeaeadeb31a585530cd5a5cc164519
muharamrizal2 <muharamrizal2@gmail.com>
2021-10-18 22:28:52 +0700

    login page component

--- src/page/login.tsx
+++ src/page/login.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import { Button, TextField, Typography } from '@mui/material';
 
 const Login = () => {
   return (
@@ -17,11 +18,45 @@ const Login = () => {
           sx={{
             width: 500,
             height: 500,
-            border: 2,
+            border: '2px solid blue',
             borderRadius: 5,
-            borderStyle: 'solid',
+            fontFamily: 'Roboto',
           }}
-        />
+        >
+          <Grid
+            container
+            direction='column'
+            justifyContent='center'
+            alignItems='center'
+            spacing={3}
+          >
+            <Grid item mt={10}>
+              <Typography variant='h3'>Student Login</Typography>
+            </Grid>
+            <Grid item container direction='column' alignItems='center' mt={5}>
+              <TextField
+                required
+                id='filled-basic'
+                label='Username'
+                sx={{ width: '80%' }}
+              />
+            </Grid>
+            <Grid item container direction='column' alignItems='center'>
+              <TextField
+                required
+                id='filled-basic'
+                label='Password'
+                type='password'
+                sx={{ width: '80%' }}
+              />
+            </Grid>
+            <Grid item container direction='column' alignItems='center'>
+              <Button variant='contained' sx={{ width: '80%' }}>
+                Login
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
       </Grid>
     </Grid>
   );
