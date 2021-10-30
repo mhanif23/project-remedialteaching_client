092f0ee09f8b0d15272a5ed1dd39e18188e2db46
muharamrizal2 <muharamrizal2@gmail.com>
2021-10-31 06:39:35 +0700

    feat(NavProtected): add protected route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import './App.css';
 import AdminLogin from './page/adminLogin';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './page/login';
+import { ProtectedRoute } from './utils/protectedRoute';
 
 function App() {
+  const ProtectedRouteData = [
+    {
+      id: 1,
+      path: '/loginSiswa',
+      component: Login,
+    },
+    {
+      id: 2,
+      path: '/loginAdmin',
+      component: AdminLogin,
+    },
+  ];
   return (
-    <>
-      <AdminLogin />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Login />
+        </Route>
+        {ProtectedRouteData.map((data) => {
+          return <ProtectedRoute component={data.component} path={data.path} />;
+        })}
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/utils/protectedRoute.tsx
@@ -0,0 +1,31 @@
+import React, { Component, FC } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+interface Props {
+  component: () => JSX.Element;
+  path: string;
+}
+export const ProtectedRoute: FC<Props> = ({ component, path }) => {
+  const login = true;
+  return (
+    <Route
+      path={path}
+      render={(props) => {
+        if (login !== true) {
+          console.log(props);
+          return <Component {...props} />;
+        } else {
+          return (
+            <Redirect
+              to={{
+                pathname: '/',
+                state: {
+                  from: props.location,
+                },
+              }}
+            />
+          );
+        }
+      }}
+    />
+  );
+};
